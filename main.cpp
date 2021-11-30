8c71daead55bb19c88c114e665a052fc1469520f
verebecske <csuha.julia@gmail.com>
2021-11-30 01:40:57 +0100

    change main

--- main.cpp
+++ main.cpp
@@ -2,9 +2,10 @@
 
 #include <iostream>
 #include "src/headers/caff.h"
+#include <string.h>
 
 int main(int argc, char* argv[]) {
-    if (argc != 3) {
+    if (argc != 2) {
         std::cerr << "Not enough arguments. Add the path of the CAFF "
             << "you want to parse and then add the path where "
             << "you want to save an image from it.";
@@ -12,6 +13,18 @@ int main(int argc, char* argv[]) {
     }
     Caff CaffObj(argv[1]);
     CaffObj.parseCaff();
-    CaffObj.saveAsImage(argv[2], 1);
+
+    const char* path = argv[1];
+    size_t path_len = strlen(path);
+    char one[path_len];
+    strcpy(one, path);
+    one[path_len - 5] = '\0';
+
+    char buf[path_len];
+    strcpy(buf, one);
+    strcat(buf, ".bmp");
+
+
+    CaffObj.saveAsImage(buf, 1);
     return 0;
 }
