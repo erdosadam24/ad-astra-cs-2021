8f74d9238e84cb72906cf0a60ca3168760142c74
Nagy Benedek <nagy.benedek96@gmail.com>
2021-11-30 15:35:28 +0100

    Skeleton with dummy data

--- CAFF_Store/CAFF_Store/ClientApp/src/app/app.module.ts
+++ CAFF_Store/CAFF_Store/ClientApp/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { Injectable, NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
@@ -8,13 +8,9 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-import { UserDetailSettingComponent } from './components/profile/user-detail-setting/user-detail-setting.component';
-import { ProfileComponent } from './components/profile/user-detail/profile.component';
-import { RegistrationComponent } from './components/registration/registration.component';
-import { SearchComponent } from './components/search/search.component';
+import { RegistrationComponent } from './components/user/registration/registration.component';
 import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './routing/app-routing.module';
-import { LoginComponent } from './components/login/login.component';
 import { AuthorizeInterceptor } from './services/user-detail/auth-interceptor/authorize.interceptor';
 
 import { MatSnackBarModule } from '@angular/material/snack-bar';
@@ -22,18 +18,41 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
+import { LoginComponent } from './components/user/login/login.component';
+import { UserDetailSettingComponent } from './components/user/profile/user-detail-setting/user-detail-setting.component';
+import { ProfileComponent } from './components/user/profile/user-detail/profile.component';
+import { FileSearchComponent } from './components/content/file-search/file-search.component';
+import { FileCardComponent } from './components/content/file-card/file-card.component';
+import { CommentEditorComponent } from './components/content/file-view/comment-editor/comment-editor.component';
+import { CommentComponent } from './components/content/file-view/comment/comment.component';
+
+import { DlDateTimeDateModule, DlDateTimePickerModule } from 'angular-bootstrap-datetimepicker';
+import { QuillModule } from 'ngx-quill';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PaginationConfig } from './directive/pagination-config';
+import { FileViewComponent } from './components/content/file-view/file-view.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FileUploadComponent } from './components/content/file-upload/file-upload.component';
+import { FileUploadModule } from 'ng2-file-upload';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    FooterComponent,
+    PageNotFoundComponent,
     LoginComponent,
     RegistrationComponent,
-    SearchComponent,
-    FooterComponent,
     ProfileComponent,
-    PageNotFoundComponent,
-    UserDetailSettingComponent
+    UserDetailSettingComponent,
+    FileSearchComponent,
+    FileCardComponent,
+    FileViewComponent,
+    CommentComponent,
+    CommentEditorComponent,
+    FileUploadComponent,
+
   ],
   imports: [
     MatSnackBarModule,
@@ -41,25 +60,27 @@ import { MatSelectModule } from '@angular/material/select';
     MatDialogModule,
     MatFormFieldModule,
     MatSelectModule,
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     CommonModule,
+    BrowserAnimationsModule,
     ReactiveFormsModule,
     AppRoutingModule,
+    FileUploadModule,
     FormsModule,
+    DlDateTimeDateModule, 
+    DlDateTimePickerModule,
+    QuillModule.forRoot(),
+    NgbModule,
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthorizeInterceptor, multi: true }
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents:[
+    FileUploadComponent
+  ]
 })
 export class AppModule { }
 
-/*
 
-    MatSnackBarModule,
-    MatPaginatorModule,
-    MatDialogModule,
-    MatFormFieldModule,
-    MatSelectModule,
-*/
