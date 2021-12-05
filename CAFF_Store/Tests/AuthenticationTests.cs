32cff71d9f1abdb353a87269f86db46bc5da3ffe
Fehérvári Attila Dávid <djindic@gmail.com>
2021-12-05 12:13:12 +0100

    register test added

--- CAFF_Store/Tests/AuthenticationTests.cs
+++ CAFF_Store/Tests/AuthenticationTests.cs
@@ -1,10 +1,16 @@
+using CAFF_Store.Data;
+using CAFF_Store.Models;
 using IdentityModel;
 using IdentityServer4;
 using IdentityServer4.Contrib.AspNetCore.Testing.Builder;
 using IdentityServer4.Contrib.AspNetCore.Testing.Configuration;
 using IdentityServer4.Contrib.AspNetCore.Testing.Services;
+using IdentityServer4.EntityFramework.Options;
 using IdentityServer4.Models;
 using IdentityServer4.Validation;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Security.Claims;
@@ -35,8 +41,19 @@ namespace Tests
         }
 	}
 
-	public class AuthenticationTests
+    public class AuthenticationTests
 	{
+        private UserManager<ApplicationUser> userManager;
+		private SignInManager<ApplicationUser> signInManager;
+
+	
+        public AuthenticationTests()
+		{
+            var userStore = new UserStore<ApplicationUser>(new IdentityDbContext<ApplicationUser>(new DbContextOptionsBuilder().UseInMemoryDatabase("testdb").Options));
+            userManager = new UserManager<ApplicationUser>(userStore, null, new PasswordHasher<ApplicationUser>(), null, null, null, null, null, null);
+
+        }
+
 		[Fact]
 		public async Task SuccesfulTokenRequestTestAsync()
 		{
@@ -105,5 +122,23 @@ namespace Tests
             Assert.NotNull(tokenResponse);
             Assert.True(tokenResponse.IsError, tokenResponse.Error ?? tokenResponse.ErrorDescription);
         }
+        [Fact]
+        public async Task RegisterTest()
+		{
+            var user = new ApplicationUser
+            {
+                Email = "testemail@email.com",
+                UserName = "testuser"
+            };
+
+            var result = await userManager.CreateAsync(user, "testpassword");
+
+            Assert.NotNull(result);
+            Assert.True(result.Succeeded);
+            var registeredUser = await userManager.FindByEmailAsync("testemail@email.com");
+            Assert.NotNull(registeredUser);
+            Assert.True(registeredUser.UserName == "testuser");
+
+		}        
     }
 }
